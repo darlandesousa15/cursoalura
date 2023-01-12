501234a760e86400039507c62ccf8776dd7fec05
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-12 06:40:46 -0300

    Adicionando outras teclas

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-function teclaSomPom() {
+function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla tecla_pom');
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+console.log(listaDeTeclas);
+
+listaDeTeclas{0}.onclick = tocaSomPom;
+listaDeTeclas{1}.onclick = tocaSomPom;
+listaDeTeclas{2}.onclick = tocaSomPom;
+listaDeTeclas{3}.onclick = tocaSomPom;
+listaDeTeclas{4}.onclick = tocaSomPom;
+listaDeTeclas{5}.onclick = tocaSomPom;
+listaDeTeclas{6}.onclick = tocaSomPom;
+listaDeTeclas{7}.onclick = tocaSomPom;
+listaDeTeclas{8}.onclick = tocaSomPom;
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
