769a2eb716c31117f5891b68cb4f005b09c46ba4
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-14 11:22:17 -0300

    Alterando main.js função while para for, ficando mais enxuta

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function tocaSom(idElementAudio) {
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while (contador < listaDeTeclas.length){
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -14,5 +14,5 @@ while (contador < listaDeTeclas.length){
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
-    contador ++;
+    
 }
